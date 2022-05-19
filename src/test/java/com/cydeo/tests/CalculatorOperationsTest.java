ce40534a42704f17aef249dc69f10550a0a4a9bd
Murodil <murodil@cybertekschool.com>
2022-05-19 15:35:54 -0400

    New test

--- src/test/java/com/cydeo/tests/CalculatorOperationsTest.java
+++ src/test/java/com/cydeo/tests/CalculatorOperationsTest.java
@@ -1,5 +1,41 @@
 package com.cydeo.tests;
 
-public class CalculatorOperationsTest {
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class CalculatorOperationsTest {
+    AppiumDriver<MobileElement> driver;
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        // caps.setCapability("deviceName", "Pixel 3");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 3");
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        caps.setCapability(MobileCapabilityType.APP, "https://cybertek-appium.s3.amazonaws.com/calculator.apk" );
+
+        //set URL for the appium server
+        URL url = new URL("http://localhost:4723/wd/hub");
+
+        //launch appiumDriver
+        driver = new AndroidDriver<MobileElement>(url, caps);
+    }
+
+    @Test
+    public void addTest() {
+
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        driver.closeApp();
+    }
 }
