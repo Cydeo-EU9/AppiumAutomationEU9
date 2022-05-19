81a3070d815c07b98a5da8882236e1626b43d3ed
Murodil <murodil@cybertekschool.com>
2022-05-19 15:50:16 -0400

    elems located

--- src/test/java/com/cydeo/tests/CalculatorOperationsTest.java
+++ src/test/java/com/cydeo/tests/CalculatorOperationsTest.java
@@ -1,16 +1,19 @@
 package com.cydeo.tests;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CalculatorOperationsTest {
+
     AppiumDriver<MobileElement> driver;
 
     @BeforeEach
@@ -29,8 +32,26 @@ public class CalculatorOperationsTest {
         driver = new AndroidDriver<MobileElement>(url, caps);
     }
 
+    /**
+     * 4 + 5 = 9
+     */
+
     @Test
     public void addTest() {
+        //locate 4 ,plus, 5, equals, and result
+        MobileElement four = driver.findElement(By.id("com.google.android.calculator:id/digit_4"));
+        four.click();
+
+        MobileElement plus = driver.findElement(MobileBy.AccessibilityId("plus"));
+        plus.click();
+
+        MobileElement five = driver.findElement(By.id("com.google.android.calculator:id/digit_5"));
+        five.click();
+
+        MobileElement equals = driver.findElement(MobileBy.AccessibilityId("equals"));
+        equals.click();
+
+
 
     }
 
