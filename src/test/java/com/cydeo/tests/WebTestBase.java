e252bf173b3d203caad88bfc587cd817de171a99
Murodil <murodil@cybertekschool.com>
2022-05-20 11:48:48 -0400

    web test

---
+++ src/test/java/com/cydeo/tests/WebTestBase.java
@@ -0,0 +1,37 @@
+package com.cydeo.tests;
+
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public abstract class WebTestBase {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 3");
+        desiredCapabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+
+
+        URL url = new URL("http://localhost:4723/wd/hub");
+        driver = new RemoteWebDriver(url, desiredCapabilities);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
