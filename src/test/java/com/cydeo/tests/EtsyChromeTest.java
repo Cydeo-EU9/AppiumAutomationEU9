fa5e29fa71c173e4a59d62f87e02172ea2d6b3d2
Murodil <murodil@cybertekschool.com>
2022-05-20 12:01:43 -0400

    web test

--- src/test/java/com/cydeo/tests/EtsyChromeTest.java
+++ src/test/java/com/cydeo/tests/EtsyChromeTest.java
@@ -1,6 +1,9 @@
 package com.cydeo.tests;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
 public class EtsyChromeTest extends WebTestBase {
 
@@ -8,6 +11,8 @@ public class EtsyChromeTest extends WebTestBase {
     public void etsySearchTest() throws InterruptedException {
         driver.get("https://www.etsy.com");
         Thread.sleep(3000);
+        WebElement searchField = driver.findElement(By.name("search_query"));
+        searchField.sendKeys("wooden spoon" + Keys.ENTER);
     }
 
 }
